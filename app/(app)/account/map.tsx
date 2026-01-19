7bc314614b5f92acc58cb3737751d143c8e8e79c
Derek Stephen McLean <delboy1978uk@gmail.com>
2026-01-19 13:50:04 +0100

    update lock

--- app/(app)/account/map.tsx
+++ app/(app)/account/map.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-// import MapScreen from '@boneframework/native-components/screens/MapScreen';
+import MapScreen from '@boneframework/native-components/screens/MapScreen';
 
 function Map(props: any) {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Map screen will be available after rebuilding the dev client</Text>
-        </View>
+        <MapScreen {...props}></MapScreen>
     );
 }
 
